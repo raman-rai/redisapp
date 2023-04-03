83172106ff6b5ddb40ad9cd66c3e243be49764cb
Raman Rai <ramankumarrai7@gmail.com>
2023-04-03 13:37:47 +0530

    getter/setter in user service

--- src/main/java/com/raman/redisapp/services/UserService.java
+++ src/main/java/com/raman/redisapp/services/UserService.java
@@ -1,7 +1,26 @@
 package com.raman.redisapp.services;
 
+import com.raman.redisapp.repository.RedisCacheRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private RedisCacheRepository redisCacheRepository;
+
+    public String getUserContact(String userId) {
+        return redisCacheRepository.get(userId);
+    }
+
+    public void updateUserContact(String userId, String phone) {
+        redisCacheRepository.set(userId, phone);
+    }
+
+    public void updateUserContactWithTTL(String userId, String phone) {
+        redisCacheRepository.setWithTTL(userId, phone, 5, TimeUnit.MINUTES);
+    }
 }
