e1a3d19c6ff52320aa0a45d08e276400a42e7253
Raman Rai <ramankumarrai7@gmail.com>
2023-04-04 10:14:22 +0530

    user controller updated

--- src/main/java/com/raman/redisapp/controller/UserController.java
+++ src/main/java/com/raman/redisapp/controller/UserController.java
@@ -1,7 +1,29 @@
 package com.raman.redisapp.controller;
 
-import org.springframework.stereotype.Controller;
+import com.raman.redisapp.entity.User;
+import com.raman.redisapp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping(value = "/v1/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "/phone/{userId}")
+    public String getUserContact(@PathVariable("userId") String userId) {
+        return userService.getUserContact(userId);
+    }
+
+    @PostMapping
+    public void updateUserContact(@RequestBody User user) {
+        userService.updateUserContact(user.getUserId(), user.getPhone());
+    }
+
+    @PostMapping
+    public void updateUserContactWithTTL(@RequestBody User user) {
+        userService.updateUserContactWithTTL(user.getUserId(), user.getPhone());
+    }
 }
--- src/main/java/com/raman/redisapp/entity/User.java
+++ src/main/java/com/raman/redisapp/entity/User.java
@@ -1,7 +1,9 @@
 package com.raman.redisapp.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class User {
     private String userId;
